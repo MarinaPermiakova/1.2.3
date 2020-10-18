a8a323b69ec9ad43671187e2676368c5e6065fed
Marina Permiakova <marina.mk.glazov@gmail.com>
2020-10-18 11:18:46 +0400

    fixed errors

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,26 @@
 fun main() {
-    val itemPrice = 1000
-    val itemCount = 11
-    val discount = 100
-    val discountStart = 1_000
-    val discountMax = 10_000
-    val totalPrice = itemPrice * itemCount
-    val result =
-        if ((totalPrice > discountStart) && (totalPrice < discountMax)) totalPrice - discount
-        else if (totalPrice > discountMax) totalPrice - (totalPrice * 0.05)
+    val itemPrice = 100.0
+    val itemCount = 1
+    var totalPrice = itemPrice * itemCount
+
+    val previousMonthPurchase = 10_001.0
+    val regularCustomer = true
+    val discountForRegularCustomer = 0.01
+
+    val discountMin = 1_000.0
+    val discountMinAmount = 100.0
+
+    val discountMax = 10_000.0
+    val discountMaxAmount = 0.05
+
+    totalPrice =
+        if ((previousMonthPurchase > discountMin) && (previousMonthPurchase < discountMax))
+            totalPrice - discountMinAmount
+        else if (previousMonthPurchase > discountMax) totalPrice - (totalPrice * discountMaxAmount)
         else totalPrice
+
+    val result = if (regularCustomer) totalPrice - (totalPrice * discountForRegularCustomer)
+    else totalPrice
+
     println("Total Price: $result")
 }
